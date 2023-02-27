4aa1b86eb3226cd0cc43c67fbd48f3e67340e85d
Florian <florian@technotro.com>
2023-02-27 22:05:25 +0100

    impl elgamal on ec

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { Group, PublicKey, Scalar, PrivateKey, shutdown } from 'snarkyjs';
+
+export { ElGamalECC };
+
+class ElGamalECC {
+  private static G = Group.generator;
+
+  static encrypt(msg: Group, y: PublicKey) {
+    let k = Scalar.random();
+    return {
+      c: this.G.scale(k),
+      d: y.toGroup().scale(k).add(msg),
+    };
+  }
+
+  static decrypt({ c, d }: { c: Group; d: Group }, priv: PrivateKey) {
+    let x = priv.s;
+    let c_ = c.scale(x);
+    let pm = d.sub(c_);
+    return pm;
+  }
+}
+
+shutdown();
