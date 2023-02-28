fa87bda2db789c8f6d1594f8df12a4ceada5aee6
Florian <florian@technotro.com>
2023-02-28 02:59:37 +0100

    add readme

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
-import {
-  SmartContract,
-  method,
-  Field,
-  isReady,
-  Poseidon,
-  shutdown,
-} from 'snarkyjs';
-import { modExp } from './lib.js';
-
 export { ElGamalECC } from './elgamal.js';
 export { modExp } from './lib.js';
