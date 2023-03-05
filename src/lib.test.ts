e3a456c66bca0f6d3715879e4b47e740e76c626a
Florian <florian@technotro.com>
2023-03-05 04:41:53 +0100

    add tests

---
+++ src/lib.test.ts
@@ -0,0 +1,65 @@
+import { isReady, shutdown, Field } from 'snarkyjs';
+import { modExp } from './lib';
+
+describe('lib', () => {
+  beforeAll(async () => {
+    await isReady;
+  });
+
+  afterAll(() => {
+    setTimeout(() => shutdown(), 0);
+  });
+
+  describe('modExp', () => {
+    // this works fine for small values, but will get tricky for huge ones
+    it('Should calculate g^x', () => {
+      let base = 5;
+      let exponent = 15;
+      modExp(Field(base), Field(exponent)).assertEquals(
+        Field(base ** exponent)
+      );
+    });
+    it('Should calculate g^x', () => {
+      let base = 85;
+      let exponent = 8;
+      modExp(Field(base), Field(exponent)).assertEquals(
+        Field(base ** exponent)
+      );
+    });
+    it('Should calculate g^x', () => {
+      let base = 71;
+      let exponent = 3;
+      modExp(Field(base), Field(exponent)).assertEquals(
+        Field(base ** exponent)
+      );
+    });
+    it('Should calculate g^x', () => {
+      let base = 1;
+      let exponent = 32;
+      modExp(Field(base), Field(exponent)).assertEquals(
+        Field(base ** exponent)
+      );
+    });
+    it('Should calculate g^x', () => {
+      let base = 2;
+      let exponent = 32;
+      modExp(Field(base), Field(exponent)).assertEquals(
+        Field(base ** exponent)
+      );
+    });
+    it('Should calculate g^x', () => {
+      let base = 32;
+      let exponent = 2;
+      modExp(Field(base), Field(exponent)).assertEquals(
+        Field(base ** exponent)
+      );
+    });
+    it('Should calculate g^x', () => {
+      let base = 70;
+      let exponent = 2;
+      modExp(Field(base), Field(exponent)).assertEquals(
+        Field(base ** exponent)
+      );
+    });
+  });
+});
